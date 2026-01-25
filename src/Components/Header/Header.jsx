76ccb4b133c1f1c6b2ca73b950e206cfaa53e33e
snowmandidi <snowmandidi@hotmail.com>
2026-01-25 13:55:23 +0100

    Initial start page and setup

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css"
+
+function Header() {
+
+    return(
+        <header class="top-bar">
+            <nav>
+                <a>Home</a>
+                <a>Experience</a>
+                <a>Contact</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
---
+++ src/Components/Introduction/Introduction.jsx
@@ -0,0 +1,15 @@
+import "./Introduction.css"
+
+function Introduction() {
+
+    return (
+        <section class="intro-section">
+            <div class="intro-text">
+                <h1>Hi, I'm Snow</h1>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+            </div>
+        </section>
+    );
+}
+
+export default Introduction
