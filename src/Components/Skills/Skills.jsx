497420cda5543b76359bab5eb5492639d67c1a99
snowmandidi <snowmandidi@hotmail.com>
2026-01-26 11:34:54 +0100

    Added cybersecurity skills

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,10 +1,12 @@
 import "./Skills.css"
 
-import { SiJavascript, SiLua, SiHtml5, SiCss3, SiNodedotjs, SiReact, SiGit} from 'react-icons/si';
+import { SiJavascript, SiLua, SiHtml5, SiCss3, SiNodedotjs, SiReact, SiGit, SiMetasploit} from 'react-icons/si';
+import { FaDragon } from "react-icons/fa";
 
 const skills = {
     Programming: [{name: "Javascript", icon: <SiJavascript/>}, {name: "Lua", icon: <SiLua/>}],
     Web: [{name: "HTML", icon: <SiHtml5/>}, {name: "CSS", icon: <SiCss3/>},  {name: "NodeJS", icon: <SiNodedotjs/>}, {name: "React", icon:  <SiReact/>}],
+    Cybersecurity: [{name: "Metasploit", icon: <SiMetasploit/>}, {name: "Hydra", icon: <FaDragon/>}],
     Other: [{name: "Git", icon: <SiGit/>}]
 }
 
